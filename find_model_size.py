a1002061e721b0721e211be10db33df6a6def5d6
samsutherland <sam.sutherland@hotmail.co.uk>
2025-10-19 22:53:57 +1100

    added memory safeguards

--- find_model_size.py
+++ find_model_size.py
@@ -32,7 +32,13 @@ train_cfg = parse(train_cfg_path.read_text(encoding="utf-8"))
 def calculate_tokens_per_parameter(num_layers, model_cfg, data_cfg, train_cfg):
     model_cfg["global"]["num_layers"] = num_layers
     model_cfg, data_cfg, train_cfg = find_batch_size(model_cfg, data_cfg, train_cfg)
+    torch.cuda.synchronize()
+    torch.cuda.empty_cache()
+    gc.collect()
     model_cfg, data_cfg, train_cfg, tokens_per_param = find_step_count(model_cfg, data_cfg, train_cfg)
+    torch.cuda.synchronize()
+    torch.cuda.empty_cache()
+    gc.collect()
     print(f"num layers {num_layers} gets {tokens_per_param} tokens per parameter.")
     return model_cfg, data_cfg, train_cfg, tokens_per_param
 
@@ -40,6 +46,9 @@ num_layers = 1
 results = {}
 while True:
     model_cfg, data_cfg, train_cfg, tokens_per_param = calculate_tokens_per_parameter(num_layers, model_cfg, data_cfg, train_cfg)
+    torch.cuda.synchronize()
+    torch.cuda.empty_cache()
+    gc.collect()
     results[num_layers] = (copy.deepcopy(model_cfg), copy.deepcopy(data_cfg), copy.deepcopy(train_cfg))
     if tokens_per_param < train_cfg["tokens_per_param"]:
         break
@@ -53,6 +62,9 @@ while upper_bound - lower_bound > 1:
     num_layers = lower_bound + (upper_bound - lower_bound) // 2
     model_cfg, data_cfg, train_cfg, tokens_per_param = calculate_tokens_per_parameter(num_layers, model_cfg, data_cfg,
                                                                                       train_cfg)
+    torch.cuda.synchronize()
+    torch.cuda.empty_cache()
+    gc.collect()
     results[num_layers] = (copy.deepcopy(model_cfg), copy.deepcopy(data_cfg), copy.deepcopy(train_cfg))
     if tokens_per_param > train_cfg["tokens_per_param"]:
         lower_bound = num_layers
