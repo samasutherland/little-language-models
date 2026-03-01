bb5cc1af1fb6a17d4295e3401e9fd5bfe8079b75
samsutherland <sam.sutherland@hotmail.co.uk>
2026-03-01 23:28:23 +0700

    added metric tracking aimlogger

--- lib/training_components/loggers.py
+++ lib/training_components/loggers.py
@@ -1,19 +1,26 @@
-# from typing import Literal, Annotated, Union, Any
-# from pydantic import Field
+from typing import Literal, Annotated, Union, Any
+from pydantic import Field
+import functools
+
+from aim import Run, Text
+from lib import Context, Factory
+
+import torch
+from torch import nn
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LRScheduler
+
+from lib.training_components import OptimizerFactory
 #
-# from aim import Run, Text
-# from lib import Context, Factory
-#
-# import torch
-# from torch import nn
-# from torch.optim import Optimizer
-# from torch.optim.lr_scheduler import LRScheduler
-#
-# from lib.training_components import OptimizerFactory
-#
-# class AimLogger(Run):
-#     def __init__(self, experiment_name: str, configs: dict):
-#         super().__init__(experiment=experiment_name)
-#         for key, value in configs.items():
-#             self[key] = value
-#
+class AimLogger(Run):
+    def __init__(self, experiment_name: str, configs: dict):
+        super().__init__(experiment=experiment_name)
+        for key, value in configs.items():
+            self[key] = value
+
+    def track_metrics(self, metrics, step, context):
+        for key, value in metrics.items():
+            self.track(value, name=key, step=step, context=context)
+
+    track_train_metrics = functools.partial(track_metrics, context={"subset": "train"})
+    track_val_metrics = functools.partial(track_metrics, context={"subset": "val"})
