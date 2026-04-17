679f5ced4104341f2dd00da061b48d94b6d41fe3
samsutherland <sam.sutherland@hotmail.co.uk>
2026-04-17 22:30:45 +1000

    vocab size 0.291k

--- scripts/train_tokenizers.py
+++ scripts/train_tokenizers.py
@@ -2,6 +2,7 @@ import sentencepiece as spm
 from datasets import load_dataset
 import itertools
 import tqdm
+import numpy as np
 
 data = load_dataset("BabyLM-community/BabyLM-2026-Strict", split="train")
 n = 10000000
@@ -13,15 +14,15 @@ def doc_stream():
         if s:
             yield s
 
-vocab_counts = [1, 2, 3, 4, 6, 8, 10]
+vocab_counts = [0.291, 0.500, 0.75]#[1, 2, 3, 4, 6, 8, 10]
 
 for vocab_size in tqdm.tqdm(vocab_counts):
     spm.SentencePieceTrainer.train(
         sentence_iterator=doc_stream(),
         model_prefix=f"baby_unigram_{vocab_size}K",
-        vocab_size=vocab_size * 1000,
+        vocab_size=int(vocab_size * 1000),
         normalization_rule_name="nmt_nfkc_cf",
-        character_coverage=1.0,
+        character_coverage=0.98,
         byte_fallback=True,
         unk_id=0,
         bos_id=1,
@@ -29,7 +30,7 @@ for vocab_size in tqdm.tqdm(vocab_counts):
         pad_id=3
     )
 
-vocab_counts = [1, 2, 3, 4, 6, 8, 10]
+vocab_counts = np.logspace(-1, 0, 4)[:3]#[1, 2, 3, 4, 6, 8, 10]
 GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
 
 for vocab_size in tqdm.tqdm(vocab_counts):
@@ -37,9 +38,9 @@ for vocab_size in tqdm.tqdm(vocab_counts):
         sentence_iterator=doc_stream(),
         model_type="bpe",
         model_prefix=f"baby_bpe_{vocab_size}K",
-        vocab_size=vocab_size * 1000,
+        vocab_size=int(vocab_size * 1000),
         normalization_rule_name="nmt_nfkc_cf",
-        character_coverage=1.0,
+        character_coverage=0.98,
         byte_fallback=True,
         unk_id=0,
         bos_id=1,
